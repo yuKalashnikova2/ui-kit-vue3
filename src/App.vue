d39738e446accad7b6bf3de24ae5158aadfd15e1
yuKalasnikova2 <pmalian@list.ru>
2024-03-18 10:39:45 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import Pagination from './components/Pagination.vue';
+</script>
+
+<template>
+  <div class="container">
+    <div class="wrapper">
+      <h1>Vue 3</h1>
+    <div>
+    <Pagination />
+    </div>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    margin: 0 auto;
+    background-color: #FFF;
+    border-radius: 12px;
+    width: 1280px;
+
+  }
+  .wrapper {
+    width: 80%;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h2>Pagination Component</h2>
+
+    <div>
+        
+    </div>
+</template>
